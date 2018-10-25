2b29a67c55d928382a6140cd673d409646c947be
Eliseo Rocha <eliseorocha@Eliseos-MacBook-Pro.local>
2018-10-25 13:47:59 -0700

    Initial commit

---
+++ typescript/cookieHelper.ts
@@ -0,0 +1,11 @@
+
+// 20 cookies per domain
+// joeswebsite.com      :one domain
+// dev.joeswebsite.com  :another domain
+// 4kb of string data per cookie
+// Cookies get sent back and forth with each request
+
+//EVERY COOKIE: Name, Value, Experation Date
+function setCookie(cookieName:string, cookieValue:string, daysTilExpire:number){
+    
+}
